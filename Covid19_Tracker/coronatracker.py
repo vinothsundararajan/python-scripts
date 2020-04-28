f28aa56ffcd121cfa65dce6ad53e40123bfa0f78
Vinoth Sundararajan <Vinoth Sundararajan@tsi.com>
2020-04-28 19:09:31 +0530

    NOTIFICATION UPDATED FOR WINDOWS

--- Covid19_Tracker/coronatracker.py
+++ Covid19_Tracker/coronatracker.py
@@ -7,10 +7,13 @@ toaster = ToastNotifier()
 def main():
     covid_India=requests.get('https://api.covid19india.org/state_district_wise.json')
     data_covid=covid_India.json()
-    Pudukkottai=data_covid['Tamil Nadu'] ['districtData'] ['Pudukkottai']
-    Chennai=data_covid['Tamil Nadu'] ['districtData'] ['Chennai']
-    Total=("Chennai: {} Pudukkottai: {}".format(Chennai,Pudukkottai))
-    toaster.show_toast(title="Corona Live Status",msg=Total)
+    Pudukkottai=data_covid['Tamil Nadu'] ['districtData'] ['Pudukkottai'] ['active']
+    TotalPudukkottai=data_covid['Tamil Nadu'] ['districtData'] ['Pudukkottai'] ['confirmed']
+    Chennai=data_covid['Tamil Nadu'] ['districtData'] ['Chennai'] ['active']
+    TotalChennai=data_covid['Tamil Nadu'] ['districtData'] ['Chennai'] ['confirmed']
+    Current_Active=("Chennai Total Case: {} \nChennai Total Case: {}".format(TotalChennai,Chennai))
+    Total_Confirmed=("Pudukkottai Total Case: {} \nPudukkottai Active Case: {}".format(TotalPudukkottai,Pudukkottai))
+    toaster.show_toast(title="Corona Live Status",msg=("{} \n{}".format(Current_Active,Total_Confirmed)), duration=15,threaded=False)
 
 if __name__ == "__main__":
-    main()    
+    main()     
