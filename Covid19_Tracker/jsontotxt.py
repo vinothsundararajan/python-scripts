09db67020c6ed37af6fc6c05907b1bf36d62d469
vinothssv <Vinoth Sundararajan@tsi.com>
2020-05-02 17:05:10 +0530

    PYTHON: Script added to track/check covid api url simple forloop

--- Covid19_Tracker/jsontotxt.py
+++ Covid19_Tracker/jsontotxt.py
@@ -9,16 +9,9 @@ with request.urlopen('https://api.covid19india.org/state_district_wise.json') as
             state = [state]
             for districts in state:
                 activevalue = data['Tamil Nadu']['districtData'][districts]['active']
-                #print (activevalue)
-            for districts in state:
                 confvalue = data['Tamil Nadu']['districtData'][districts]['confirmed']
-                #print (confvalue)
-            for districts in state:
                 recovrvalue = data['Tamil Nadu']['districtData'][districts]['recovered']
-                #print (recovrvalue)
-            for districts in state:
                 decevalue = data['Tamil Nadu']['districtData'][districts]['deceased']
-                #print (decevalue)
-            print('{:15}\t district details \tActive:{} \tConfirmed:{} \tRecovered:{} \tDecreased:{}'.format(districts,activevalue,confvalue,recovrvalue,decevalue))
+                print('{:15}\t district details \tActive:{} \tConfirmed:{} \tRecovered:{} \tDecreased:{}'.format(districts,activevalue,confvalue,recovrvalue,decevalue))
     else:
         print("Api server unable to reach")
